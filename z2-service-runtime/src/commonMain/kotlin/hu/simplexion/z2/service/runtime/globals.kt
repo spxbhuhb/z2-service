59e2d3d575c95733991882432213e56364097d0c
Tóth István Zoltán <tiz@simplexion.hu>
2023-08-21 14:08:20 +0200

    service context data access helpers

--- z2-service-runtime/src/commonMain/kotlin/hu/simplexion/z2/service/runtime/globals.kt
+++ z2-service-runtime/src/commonMain/kotlin/hu/simplexion/z2/service/runtime/globals.kt
@@ -1,13 +1,14 @@
 package hu.simplexion.z2.service.runtime
 
+import hu.simplexion.z2.commons.util.UUID
 import hu.simplexion.z2.service.runtime.factory.BasicServiceImplFactory
 import hu.simplexion.z2.service.runtime.factory.ServiceImplFactory
 import hu.simplexion.z2.service.runtime.transport.LocalServiceCallTransport
 import hu.simplexion.z2.service.runtime.transport.ServiceCallTransport
 
-var defaultServiceCallTransport : ServiceCallTransport = LocalServiceCallTransport()
+var defaultServiceCallTransport: ServiceCallTransport = LocalServiceCallTransport()
 
-val defaultServiceImplFactory : ServiceImplFactory = BasicServiceImplFactory()
+val defaultServiceImplFactory: ServiceImplFactory = BasicServiceImplFactory()
 
 /**
  * Get a service consumer for the interface, specified by the type parameter.
@@ -18,6 +19,22 @@ val defaultServiceImplFactory : ServiceImplFactory = BasicServiceImplFactory()
  * val clicks = getService<ClickApi>()
  * ```
  */
-fun <T : Service> getService(consumer : T? = null) : T {
+fun <T : Service> getService(consumer: T? = null): T {
     return checkNotNull(consumer)
+}
+
+/**
+ * Get a typed data instance fom the service context.
+ *
+ * @throws  ClassCastException  If the instance is not of the class [T].
+ */
+inline operator fun <reified T> ServiceContext?.get(uuid: UUID<T>): T? {
+    return this?.data?.let { it[uuid] } as T
+}
+
+/**
+ * Put a data instance into the service context.
+ */
+operator fun <T> ServiceContext?.set(uuid: UUID<T>, data: T) {
+    this?.data?.let { it[uuid] = data }
 }
